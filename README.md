d005bcd764da03d6926d6608eb1f2c81e56122c1
Stefan Seide <stefan@trilobyte-se.de>
2019-01-25 17:21:07 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,201 @@
 # jsdoc-plantuml
-jsdoc3 plugin to use plantuml inside javascript documentation
+This package contains a jsdoc3 plugin to use plantuml inside javascript documentation.
+
+* JSDoc: http://usejsdoc.org (Node-Package: https://www.npmjs.com/package/jsdoc) 
+* PlantUML: http://plantuml.com/
+
+This plugin allows using the plantuml syntax with "@startuml ... @enduml" to use inside
+your normal JSDoc source code comments. This plugins parses them and either writes it 
+into seperate failes for external processing/displaying in ticket systems/... or 
+generates image files to include in your generated documentation.
+
+Extracting the plantuml source into extra files works without extra dependnecies,
+Creating image files need a installation of the "node-plantuml" package as well as the
+"graphviz" tool to actually generate some of the image formats. For further documentation check
+https://www.npmjs.com/package/node-plantuml.
+
+## Usage
+
+install jsdoc and this plugin
+
+Now write some uml diagrams inside your regular jsdocs. 
+
+Attention - The `@startuml` tag must get one parameter - the filename to safe this uml diagram at.
+```js
+/**
+ *  my normal jsodc comments...
+ *  and here i reference my image generated with <img src=filename.png">
+ *  inside generate html docs
+ *
+ * @startuml filename.png
+ *  Alice --> Bob
+ * @enduml
+ *
+ * some more comments as you like...
+ */
+```
+
+The filename given after `@startuml` can either we a relativ filename with or without paths or
+an absolute filename. @startuml tags without filename are ignored by this plugin.
+
+The filename should either end with the graphic format needed (png, svg, eps) or with "puml"
+as format for the plant uml source code files. If puml is used here the defaultFormat from the
+configuration is used to determined the file format created.
+
+* `@startuml <filename>` can contain paths, allowed file formats are "png, svg, eps, puml" 
+  examples "@startuml file.png" or "@startuml my/path/file.puml" etc.pp.
+
+Inside the jsdoc configuration this plugin must be registered as `plugins` and can be optionally
+configured with a `plantuml` object. The following configuration gives the default config used
+if no plantuml object is added to your jsdoc-config.
+
+```json
+{
+  ...
+  "plugins": [ "node_modules/jsdoc-plantuml"],
+  "plantuml": {
+    "puml": {
+      "create": true,
+      "destination": "jsDoc/puml"
+    },
+    "images": {
+      "create": true,
+      "destination": "jsDoc/images",
+      "defaultFormat": "png"
+    }
+  }
+}
+```
+## Configuration parameters
+
+`puml: {}` Object containing all parameters related to the creation of the puml files from jsdoc
+comments.
+* `puml.create` Boolean flag to indicate if puml files shall be created or not
+* `puml.destination` Path (absolute or relative to working dir) where the  puml files will
+be stored. If the filename at the `@startuml` tag contains some paths too these to will
+be concatened. All files will be created with the '.puml' extension.
+
+`images: {}` Object containing all parameters related to the creation of the image files from 
+jsdoc comments.
+* `images.create` Boolean flag to indicate if image files shall be created or not
+* `images.destination` Path (absolute or relative to working dir) where the image files will
+be stored. If the filename at the `@startuml` tag contains some paths too these to will
+be concatened.
+* `images.defaultFormat` If the filename given at the `@startuml` tag ends with the '.puml'
+ file suffix this format will be used to create images. If the filename already contains a file format
+ this one is ignore.
+ 
+## Examples
+
+#### JSDoc comment with one uml diagram, only puml code file should be saved, no image
+
+jsdoc-config.json
+```json
+{
+  ...
+  "plugins": [ "node_modules/jsdoc-plantuml"],
+  "plantuml": {
+    "puml": {
+      "create": true,
+      "destination": "jsdoc/puml"
+    },
+    "images": {
+      "create": false
+    }
+  }
+}
+```   
+
+Javascript source file. The filename and format is taken from the image file name
+at the `@startuml` tag. For the puml file the filename replaces the graphics suffix ("png") by "puml"
+
+```js
+/** This file does some magic yada-blah
+  @requires 'module:yada'
+  
+  @startuml images/yadablah.png
+    Alice --> Bob
+  @enduml
+ */
+function yadablah() {
+    ...
+}
+```
+The File safed is "jsdoc/puml/images/yadablah.svg" inside current working directory.
+
+
+#### JSDoc comment with one uml diagram, only image file should be saved, not the puml file
+
+jsdoc-config.json
+```json
+{
+  ...
+  "plugins": [ "node_modules/jsdoc-plantuml"],
+  "plantuml": {
+    "puml": {
+      "create": false
+    },
+    "images": {
+      "create": true
+    }
+  }
+}
+```   
+
+Javascript source file. The filename and format is taken from the image file name
+at the `@startuml` tag.
+
+```js
+/** This file does some magic yada-blah
+  @requires 'module:yada'
+  
+  @startuml images/yadablah.png
+    Alice --> Bob
+  @enduml
+ */
+function yadablah() {
+    ...
+}
+```
+The File safed is "images/yadablah.svg" inside current working directory.
+
+#### JSDoc comment with one uml diagram, create image and puml file
+
+jsdoc-config.json, set default format for images to "svg" instead of "png"
+```json
+{
+  ...
+  "plugins": [ "node_modules/jsdoc-plantuml"],
+  "plantuml": {
+    "puml": {
+      "create": false
+    },
+    "images": {
+      "create": true,
+      "defaultFormat": "svg"
+    }
+  }
+}
+```   
+
+Javascript source file. The filename is taken from the image file name at the `@startuml` tag,
+but format/extension replaced by config param "defaultFormat" as "puml" is no image format
+
+```js
+/** This file does some magic yada-blah
+  @requires 'module:yada'
+  
+  @startuml images/yadablah.puml
+    Alice --> Bob
+  @enduml
+ */
+function yadablah() {
+    ...
+}
+```
+
+Files safed are "images/yadablah.puml" and "images/yadablah.svg" inside current working directory.
+
+
+# Copyright
+Trilobyte GmbH / Stefan Seide, 2019
---
+++ index.js
@@ -0,0 +1,197 @@
+const env = require('jsdoc/env');
+const logger = require('jsdoc/util/logger');
+const path = require('path');
+const fs = require('fs');
+const fse = require('fs-extra');
+
+const logPrefix = 'jsdoc-plantuml: ';
+
+// lazy init if needed
+let plantuml;
+
+
+// default configuration
+let myConfig = {
+    createPuml: true,
+    createImages: true,
+    pathPuml: './jsDoc/puml',
+    pathImages: './jsDoc/images',
+    imageFormat: 'png'
+};
+_checkConfiguration();
+
+// ===
+// check configuration options
+//
+// logger.warn('jsdoc env: ' + JSON.stringify(env));
+// logger.warn('jsdoc config: ' + JSON.stringify(env.conf));
+
+/** function to check for config params set via jsdoc to adapt our own default config
+ *
+ *  @private
+ */
+function _checkConfiguration() {
+    const config = env.conf.plantuml || {};
+    if (config.puml) {
+        if (typeof config.puml.create === 'boolean') myConfig.createPuml = config.puml.create;
+        if (config.puml.destination) {
+            myConfig.pathPuml = config.puml.destination;
+            logger.debug(logPrefix + 'got destination path from config: ' + myConfig.pathPuml);
+        }
+    }
+    logger.info(logPrefix + 'using destination path for puml files: ' + myConfig.pathPuml);
+
+    if (config.images) {
+        if (typeof config.images.create === 'boolean') myConfig.createImages = config.images.create;
+        if (config.images.destination) {
+            myConfig.pathImages = config.images.destination;
+            logger.debug('got destination path from config: ' + myConfig.pathImages);
+        }
+        if (config.images.defaultFormat) {
+            myConfig.imageFormat = config.images.defaultFormat;
+        }
+    }
+    logger.info(logPrefix + 'using destination path for image files: ' + myConfig.pathImages);
+}
+// end configuration
+// ====
+
+
+exports.handlers = {
+    /** function to check at beginning if needed peer dependency is installed if image files shall be created
+     *
+     * @param {object} e - jsdoc handler event (http://usejsdoc.org/about-plugins.html)
+     */
+    parseBegin: function(e) {
+        if (myConfig.createImages) {
+            try {
+                plantuml = require('node-plantuml');
+            }
+            catch (ex) {
+                logger.error(logPrefix + '"createImages" is set to TRUE but package "node-plantuml" not installed');
+            }
+        }
+    },
+    /** check all doclets creeated and extract list of uml descriptions found to either write PUML files with
+     *  their source or create image from them
+     *
+     *  @param {object} e - jsdoc handler event (http://usejsdoc.org/about-plugins.html)
+     */
+    processingComplete: function(e) {
+        logger.debug(logPrefix + 'processingComplete: doclet length=' + e.doclets.length);
+        let umlTags = [];
+        e.doclets.filter((d) => Array.isArray(d.plantUml))
+    .forEach((doc) => doc.plantUml.forEach((tag) => umlTags.push(tag)));
+
+        logger.debug(logPrefix + 'processingComplete: uml tags length=' + umlTags.length + ', ' + JSON.stringify(umlTags));
+        umlTags.forEach(function(umlTag) {
+            if (myConfig.createPuml && umlTag.outFilePuml) {
+                logger.info('writing puml file ' + umlTag.outFilePuml);
+                fse.ensureDir(path.dirname(umlTag.outFilePuml), function(err) {
+                    if (err && !err.EEXIST) {
+                        logger.error(logPrefix + 'Cannot create directory to write puml files: ' + err.toString());
+                        return;
+                    }
+                    fse.outputFile(umlTag.outFilePuml, umlTag.value.description, function(err) {
+                        if (err) logger.error(logPrefix + 'Could not write PUML file at ' + umlTag.outFilePuml + ': ' + JSON.stringify(err));
+                    });
+                });
+            }
+            if (myConfig.createImages && umlTag.outFileImage) {
+                logger.info(logPrefix + 'writing ' + umlTag.imageFormat + ' image file ' + umlTag.outFileImage);
+                fse.ensureDir(path.dirname(umlTag.outFileImage), function(err) {
+                    if (err && !err.EEXIST) {
+                        logger.error(logPrefix + 'Cannot create directory to write image files: ' + err.toString());
+                        return;
+                    }
+                    // write imagefile
+                    let gen = plantuml.generate(umlTag.value.description, {format: umlTag.imageFormat});
+                    gen.out.pipe(fs.createWriteStream(umlTag.outFileImage));
+                });
+            }
+        });
+    }
+};
+
+
+exports.defineTags = function(dictionary) {
+    // define tags here
+    dictionary.defineTag('startuml', {
+        canHaveName: true,
+        mustHaveValue: true,
+        /** define doclet for startuml to to catch all plat uml related definitions
+         *
+         *  @param {object} doclet latest doclet
+         *  @param {object} tag new tag created
+         */
+        onTagged: function(doclet, tag) {
+            doclet.plantUml = [];
+            tag.srcFile = path.join(doclet.meta.path, doclet.meta.filename);
+            tag.value.description = '@startuml\n' + tag.value.description + '\n@enduml';
+            if (tag.value.name) {
+                // check if really name is given - look for plantuml known file extensions
+                let extension = path.extname(tag.value.name);
+                logger.debug(logPrefix + 'found uml tag with file name extension ' + extension);
+                switch (extension.toLowerCase()) {
+                    case '.png':
+                    case '.svg':
+                    case '.eps':
+                        tag.outFilePuml = path.join(myConfig.pathPuml, tag.value.name.replace(new RegExp(extension + '$'), '.puml'));
+                        if (!path.isAbsolute(tag.outFilePuml)) {
+                            tag.outFilePuml = path.join(process.cwd(), tag.outFilePuml);
+                            logger.debug(logPrefix + 'new puml path not absolute - new is ' + tag.outFilePuml);
+                        }
+                        if (myConfig.pathImages) {
+                            tag.imageFormat = extension.substr(1);
+                            tag.outFileImage = path.join(myConfig.pathImages, tag.value.name);
+                            if (!path.isAbsolute(tag.outFileImage)) {
+                                tag.outFileImage = path.join(process.cwd(), tag.outFileImage);
+                            }
+                        }
+                        doclet.plantUml.push(tag);
+                        break;
+
+                    case '.puml':
+                        tag.imageFormat = myConfig.imageFormat;
+                        if (myConfig.pathPuml) {
+                            tag.outFilePuml = path.join(myConfig.pathPuml, tag.value.name);
+                            if (!path.isAbsolute(tag.outFilePuml)) {
+                                tag.outFilePuml = path.join(process.cwd(), tag.outFilePuml);
+                            }
+                        }
+                        // create image file name with default format
+                        if (myConfig.pathImages) {
+                            tag.outFileImage = path.join(myConfig.pathImages, tag.value.name.replace(new RegExp(extension + '$'), '.' + myConfig.imageFormat));
+                            if (!path.isAbsolute(tag.outFileImage)) {
+                                tag.outFileImage = path.join(process.cwd(), tag.outFileImage);
+                            }
+                        }
+                        doclet.plantUml.push(tag);
+                        break;
+
+                    default:
+                        logger.warn(logPrefix + 'IGNORED: unknown image format "' + extension + '" or image name missing for @startuml tag at doclet starting line ' +
+                            doclet.meta.lineno + ', file ' + tag.srcFile);
+                }
+            }
+            else {
+                logger.warn(logPrefix + 'IGNORED: image name missing for @startuml tag at doclet starting line ' + doclet.meta.lineno +
+                    ', file ' + tag.srcFile);
+            }
+
+            logger.debug(logPrefix + 'startuml / onTagged: tag=' + JSON.stringify(tag));
+        }
+    });
+    dictionary.defineTag('enduml', {
+        canHaveName: false,
+        mustHaveValue: false,
+        /** define doclet for enduml to get all text between startuml and enduml for writing files
+         *
+         *  @param {object} doclet latest doclet
+         *  @param {object} tag new tag created
+         */
+        onTagged: function(doclet, tag) {
+            logger.debug(logPrefix + 'enduml / onTagged: tag=' + JSON.stringify(tag));
+        }
+    });
+};
